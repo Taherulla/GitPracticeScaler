4e171422e980a9d9859ae759f476c5367fb707e0
taherulla <taherulla@gmail.com>
2024-08-01 13:52:03 +0530

    adding all files

--- practice/Calculator.java
+++ practice/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("calculator class");
+        System.out.println("adding new line in the new feature ");
     }
 }
