8e8236e36aadd8ba4aced4a8f9f957591267dd3c
taherulla <taherulla@gmail.com>
2024-08-02 17:57:17 +0530

    added new changes

--- practice/Calculator.java
+++ practice/Calculator.java
@@ -3,5 +3,6 @@ public class Calculator {
         System.out.println("calculator class");
         System.out.println("adding new line in the new feature ");
         System.out.println("Compound Interest " + InterestCalculator.calculate(100000, 8, 5));
+        System.out.println("SIP return : " + SIPCalculator.calculate(10000,12,12, 20));
     }
 }
